3ddf7698f51babb6e18a66027411c36cb6847437
Emebetyw <Emuyoha21@gmail.com>
2023-03-02 14:24:50 +0300

    add a node at the right of parent

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,37 @@
+#include "binary_trees.h"
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *pright = NULL;
+	binary_tree_t *new_node = NULL;
+	
+	if (!parent)
+		return (NULL);
+
+	new_node = malloc(sizeof(binary_tree_t));
+
+	if (!new_node)
+		return (NULL);
+
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	pright = parent->right;
+	parent->right = new_node;
+
+	if (pright)
+	{
+		pright->parent = new_node;
+		new_node->right = pright;
+	}
+	return (new_node);
+}
